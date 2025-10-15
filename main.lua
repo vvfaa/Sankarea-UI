061ffe2c7698b6bf86789459cc01a07be12011f0
vvfaa <32589109+vvfaa@users.noreply.github.com>
2025-10-15 19:42:45 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -16,7 +16,8 @@ local a, b = {
                     {51, "ModuleScript", {"Darker"}},
                     {53, "ModuleScript", {"Rose"}},
                     {49, "ModuleScript", {"Aqua"}},
-                    {48, "ModuleScript", {"Amethyst"}}
+                    {48, "ModuleScript", {"Amethyst"}},
+                    {54, "ModuleScript", {"XenryDarkBlue"}}
                 }
             },
             {
